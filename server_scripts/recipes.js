04fdb86dd2a42d5d42c677727aaaf27e5619f2ca
YukiTheFunkiest <mtkonar@gmail.com>
2026-04-05 02:11:07 -0400

    Finished casing crafting adjustment #1

--- server_scripts/recipes.js
+++ server_scripts/recipes.js
@@ -55,6 +55,118 @@ ServerEvents.recipes(e => {
     e.remove({
       id: "scguns:immersiveengineering/metalpress/medium_copper_casing"
 })
+    e.remove({
+      id: "scguns:immersiveengineering/metalpress/small_brass_casing"
+})
+    e.remove({
+      id: "scguns:immersiveengineering/metalpress/medium_brass_casing"
+})
+    e.remove({
+      id: "scguns:immersiveengineering/metalpress/large_brass_casing"
+})
+    e.remove({
+      id: "scguns:immersiveengineering/metalpress/small_iron_casing"
+})
+    e.remove({
+      id: "scguns:immersiveengineering/metalpress/large_iron_casing"
+})
+    e.remove({
+      id: "scguns:immersiveengineering/metalpress/small_diamond_steel_casing"
+})
+    e.remove({
+      id: "scguns:immersiveengineering/metalpress/medium_diamond_steel_casing"
+})
+    e.custom({
+      // Small Diamond Steel Casings
+        "type": "immersiveengineering:metal_press",
+  "energy": 2150,
+  "input": {
+    "item": "scguns:depleted_diamond_steel_ingot"
+  },
+  "mold": "scguns:small_casing_mold",
+  "result": {
+    "count": 28,
+    "item": "scguns:small_diamond_steel_casing"
+  }
+})
+    e.custom({
+      // Medium Diamond Steel Casings
+        "type": "immersiveengineering:metal_press",
+  "energy": 2150,
+  "input": {
+    "item": "scguns:depleted_diamond_steel_ingot"
+  },
+  "mold": "scguns:medium_casing_mold",
+  "result": {
+    "count": 20,
+    "item": "scguns:medium_diamond_steel_casing"
+  }
+})
+    e.custom({
+      // Small Iron Casings
+        "type": "immersiveengineering:metal_press",
+  "energy": 2150,
+  "input": {
+    "tag": "forge:ingots/iron"
+  },
+  "mold": "scguns:small_casing_mold",
+  "result": {
+    "count": 28,
+    "item": "scguns:small_iron_casing"
+  }
+})
+    e.custom({
+      // Large Iron Casings
+        "type": "immersiveengineering:metal_press",
+  "energy": 2150,
+  "input": {
+    "tag": "forge:ingots/iron"
+  },
+  "mold": "scguns:large_casing_mold",
+  "result": {
+    "count": 12,
+    "item": "scguns:large_iron_casing"
+  }
+})
+    e.custom({
+      // Small Brass Casings
+        "type": "immersiveengineering:metal_press",
+  "energy": 2150,
+  "input": {
+    "tag": "forge:ingots/brass"
+  },
+  "mold": "scguns:small_casing_mold",
+  "result": {
+    "count": 28,
+    "item": "scguns:small_brass_casing"
+  }
+})
+    e.custom({
+      // Medium Brass Casings
+        "type": "immersiveengineering:metal_press",
+  "energy": 2150,
+  "input": {
+    "tag": "forge:ingots/brass"
+  },
+  "mold": "scguns:medium_casing_mold",
+  "result": {
+    "count": 28,
+    "item": "scguns:medium_brass_casing"
+  }
+})
+    e.custom({
+      // Large Brass Casings
+        "type": "immersiveengineering:metal_press",
+  "energy": 2150,
+  "input": {
+    "tag": "forge:ingots/brass"
+  },
+  "mold": "scguns:large_casing_mold",
+  "result": {
+    "count": 12,
+    "item": "scguns:large_brass_casing"
+  }
+})
     e.custom({
       // Small Copper Casings
         "type": "immersiveengineering:metal_press",
@@ -83,33 +195,11 @@ ServerEvents.recipes(e => {
 })
 
   // I.E Custom Recipes go here (52-X)
-    e.custom({
-  "type": "immersiveengineering:blueprint",
-  "category": "bullet",
-  "inputs": [
-    {
-      "base_ingredient": {
-        "item": "scguns:small_copper_casing"
-      },
-      "count": 8
-    },
-    {
-      "base_ingredient": {
-        "item": "scguns:standard_bullet"
-      },
-      "count": 8
-    },
-    {
-      "base_ingredient": {
-        "item": "scguns:gunpowder_dust"
-      },
-      "count": 8
-    }
-  ],
-  "result": {
-    "count": 16,
-    "item": "scguns:compact_copper_round"
-  }
-})
-    e.recipes.immersiveengineering.blueprint(Item.of("scguns:hog_round", 16), [Item.of("scguns:small_iron_casing", 8), Item.of("scguns:sheol_dust", 8), Item.of("minecraft:gold_nugget", 8)], "Advanced Bullets")
+    e.recipes.immersiveengineering.blueprint(Item.of("scguns:hog_round", 16), [Item.of("scguns:small_iron_casing", 8), Item.of("scguns:sheol_dust", 8), Item.of("minecraft:gold_nugget", 8)], "Advanced Rounds")
+
+    e.recipes.immersiveengineering.blueprint(Item.of("scguns:compact_copper_round", 16), [Item.of("scguns:small_copper_casing", 8), Item.of("scguns:gunpowder_dust", 8), Item.of("scguns:standard_bullet", 8)], "Basic Rounds")
+
+//    e.recipes.immersiveengineering.blueprint(Item.of("scguns:hog_round", 16), [Item.of("scguns:small_iron_casing", 8), Item.of("scguns:sheol_dust", 8), Item.of("minecraft:gold_nugget", 8)], "Advanced Bullets")
+
+
 })
